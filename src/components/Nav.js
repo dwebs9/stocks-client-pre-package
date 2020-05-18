8d1e84385e6ec9d3f9ce1f5afd8cc97098475b60
David <me@example.com>
2020-05-18 14:00:38 +1000

    added some light styling to the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,31 +7,49 @@ import {
   NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   NavbarText,
 } from "reactstrap";
 import React, { Component, useContext } from "react";
-import navStyle from "./Nav.css";
+// import navStyle from "./Nav.css";
 import LogoutButton from "./LogoutButton";
 import LoginButton from "./LoginButton";
 
 // How to style in react
 const imgStyle = {
-  margin: 50,
+  margin: 1,
   width: "25%",
+  display: "flex",
 };
 
 function NavComp(props) {
   return (
-    <nav class="navbar navbar-expand bg-base-blue text-white py-2">
+    <Navbar>
+      <NavbarBrand href="/">
+        {/* <img src={patrick} alt="Logo" style={imgStyle} /> */}
+      </NavbarBrand>
+      {/* <Nav className="mr-auto" navbar></Nav> */}
+
+      <ul className="nav-links">
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/stock/A">
+          <li>Quote</li>
+        </Link>
+        <div>
+          {!props.isauth ? <LoginButton /> : <LogoutButton props={props} />}
+        </div>
+      </ul>
+    </Navbar>
+  );
+}
+
+export default NavComp;
+{
+  /* <nav class="navbar navbar-expand bg-base-blue text-white py-2">
       <img src={patrick} alt="Logo" style={imgStyle} />
       <ul className="nav-links">
-        <Link to="/" style={navStyle}>
+        <Link to="/">
           <li>Home</li>
         </Link>
         <Link to="/stock/A">
@@ -42,8 +60,5 @@ function NavComp(props) {
         </div>
       </ul>
       <img src={moss} alt="Logo" />
-    </nav>
-  );
+    </nav> */
 }
-
-export default NavComp;
